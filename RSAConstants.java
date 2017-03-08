5ae9b32d5b6c4e6119b80b79a8845cb39d741e3f
Andrew Martin <martin_am@wvwc.edu>
2017-03-07 23:41:08 -0500

    Add files via upload

---
+++ RSAConstants.java
@@ -0,0 +1,18 @@
+package com.rhg.rsa;
+
+import java.math.BigInteger;
+/**
+ * Interface providing the BigInteger constants.
+ * 
+ * @author Andrew
+ * @version 1
+ */
+
+public interface RSAConstants
+{
+    BigInteger ZERO = BigInteger.ZERO;
+    BigInteger ONE = BigInteger.ONE;
+    BigInteger TWO = BigInteger.valueOf(2);
+    BigInteger THREE = BigInteger.valueOf(3);
+    BigInteger TWO_FIFTY_SIX = BigInteger.valueOf(256);
+}
